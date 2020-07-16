87853d41d259c6dbd56dca47ad1232e809d756b6
Seungpang <obey1342@gmail.com>
2020-07-17 00:04:25 +0900

    전체 조회기능

--- src/main/java/com/seungpang/book/springboot/service/PostsService.java
+++ src/main/java/com/seungpang/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.seungpang.book.springboot.service;
 
 import com.seungpang.book.springboot.web.domain.posts.Posts;
 import com.seungpang.book.springboot.web.domain.posts.PostsRepository;
+import com.seungpang.book.springboot.web.dto.PostsListResponseDto;
 import com.seungpang.book.springboot.web.dto.PostsResponseDto;
 import com.seungpang.book.springboot.web.dto.PostsSaveRequestDto;
 import com.seungpang.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDese(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
